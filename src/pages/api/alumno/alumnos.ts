33dfd4bfa11fdddef696fd7ea4c1dbb30949033e
JJRoma <jj_roma@yahoo.com.ar>
2025-02-20 19:22:55 -0300

    ordenados por nombre

--- src/pages/api/alumno/alumnos.ts
+++ src/pages/api/alumno/alumnos.ts
@@ -1,16 +1,45 @@
-import { PrismaClient } from '@prisma/client';
-import { NextApiRequest, NextApiResponse } from 'next';
+// import { PrismaClient } from '@prisma/client';
+// import { NextApiRequest, NextApiResponse } from 'next';
 
+
+// const prisma = new PrismaClient();
+
+
+// export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+//   if (req.method === 'GET') {
+//     try {
+//       const alumnos = await prisma.alumno.findMany({
+//         include:{asistencia:true, Eventos:true, graduaciones:true},
+//       }); // Ajusta según tu modelo
+//       res.status(200).json(alumnos);
+//     } catch (error) {
+//       console.error(error);
+//       res.status(500).json({ error: 'Error fetching alumnos' });
+//     }
+//   } else {
+//     res.setHeader('Allow', ['GET']);
+//     res.status(405).end(`Method ${req.method} Not Allowed`);
+//   }
+// }
+import { NextApiRequest, NextApiResponse } from 'next';
+import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
-
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'GET') {
     try {
       const alumnos = await prisma.alumno.findMany({
-        include:{asistencia:true, Eventos:true, graduaciones:true},
-      }); // Ajusta según tu modelo
+        include: {
+          asistencia: true,
+          Eventos: true,
+          graduaciones: true,
+        },
+        orderBy: {
+          nombre: 'asc', // Ordenar por el campo 'nombre' de manera ascendente
+        },
+      });
+
       res.status(200).json(alumnos);
     } catch (error) {
       console.error(error);
