2ad8eca211e4f7873cb7eee1f881d5cb6789cfdc
JJRoma <jj_roma@yahoo.com.ar>
2025-03-02 10:00:07 -0300

    un error

--- src/app/ficha/addferiado/page.tsx
+++ src/app/ficha/addferiado/page.tsx
@@ -1,7 +1,6 @@
 
 'use client';
 import { useState, useEffect } from 'react';
-import styles from '../../ui/ficha.module.css';
 import { formatearFecha } from '../../funcs/funciones';
 
 interface Evento {
