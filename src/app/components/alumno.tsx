5e6dd6d53b558c544275c7e2ed0a0b09b8bcbf8b
JJRoma <jj_roma@yahoo.com.ar>
2025-03-08 17:01:56 -0300

    arreglo

--- src/app/components/alumno.tsx
+++ src/app/components/alumno.tsx
@@ -13,12 +13,12 @@ const AlumnoCard = (props: AlumnoProps)=>{
 
     return (
         
-        <main className='bg-gray-600 grid grid-cols-[15%_85%] gap-2 w-full min-h-[3rem] p-1 rounded-lg mb-1'>
-            <section className='bg-gray-400 w-full h-11 p-1 rounded flex justify-center items-center'>
+        <main className='bg-gray-200 grid grid-cols-[15%_84%] gap-2 w-full min-h-[3rem] p-1 rounded-lg mb-1'>
+            <section className='bg-gray-100 w-full h-11 p-1 rounded flex justify-center items-center'>
                 <Image className='border-black rounded-lg' src={alumno.foto} alt={alumno.nombre} width={40} height={40} />
             </section>
-            <section className='bg-gray-400 w-full h-11 px-4 flex items-center rounded' >
-                <h1 className='text-lg font-semibold text-black'>{alumno.nombre}</h1>
+            <section className='bg-gray-100 w-full h-11 px-4 flex items-center rounded' >
+                <h1 className='text-lg font-semibold text-black  w-full'>{alumno.nombre}</h1>
             </section>
 
         </main>
